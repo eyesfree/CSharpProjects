08a9dd7404353a65c93f2a40c2891d572d442230
eyesfree <kristinapetkova.tu@gmail.com>
2014-03-14 08:18:47 +0200

    2-4-8 problem from bgcoder.com
    simple task for my colleagues to check out.

---
+++ MollyMolly.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_4_8
+{
+    class MollyMolly
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+
+            int R;
+            switch(B){
+                case 2: 
+                    R = A%C;
+                    break;
+                case 4: 
+                    R=A+C;
+                    break;
+                case 8:
+                    R=A*C;
+                    break;
+                default: 
+                    Console.WriteLine("Unknown secret Code!!!");
+                    R = 0;
+                    break;
+            }
+
+            if(R%4 == 0) 
+            {
+                Console.WriteLine(R/4);
+            }
+            else 
+            {
+                Console.WriteLine(R%4);
+            }
+
+            Console.WriteLine(R);
+            Console.ReadKey();
+        }
+    }
+}
